7e6ee60846c2e54172ef33a3de9c295ef8bb02fc
belfly1111 <belfly1111@naver.com>
2023-07-22 22:45:40 +0900

    Shadow 능력생성.

--- Assets/Scripts/Stage1/Skillmanager_Stage_1.cs
+++ Assets/Scripts/Stage1/Skillmanager_Stage_1.cs
@@ -1,42 +1,126 @@
-using Photon.Pun;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEditor;
 
 //빛: 대쉬
 //어둠: 그림자
 //오브젝트: 지형생성, 반사 +) 유리오브젝트는 빛 투과하고 못지나감
 
+
 public class Skillmanager_Stage_1 : MonoBehaviourPun
 {
-    [SerializeField] private PhotonView PV;
-    
-    #region NormalFunction
-    private void CooTimeEnd()
+    public enum PlayerRole
     {
-        Debug.Log("쿨타임 끝");
+        Dark,
+        Light
     }
+
+    private PlayerRole PR;
+    public PhotonView PV;
+
+    [SerializeField] GameObject Light;
+    [SerializeField] GameObject Dark;
+
+    [SerializeField] float DashPower = 0;
+    bool skillcool;
+
+    void Start()
+    {
+        skillcool = false;
+        PV = this.GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.V))
+        {
+            if(Light == null) Light = GameObject.Find("Light(Clone)");
+            if(Dark == null) Dark = GameObject.Find("Dark(Clone)");
+            if(Dark != null && Light != null) UseSkill();
+        }
+    }
+
+
+    #region Nomalregion
+
+    //스킬 사용
+    public void UseSkill()
+    {
+        // 플레이어가 선택한게 없을 경우
+        if (StageUI_1.LocalPlayerRule == -1) return;
+
+        // 플레이어가 선택한게 'Light'인 경우
+        else if (StageUI_1.LocalPlayerRule == 1 && !skillcool)
+        {
+            StartCoroutine("Dash");
+        }
+
+        // 플레이어가 선택한게 'Dark'인 경우
+        else if (StageUI_1.LocalPlayerRule == 0 && !skillcool)
+        {
+            StartCoroutine("Teleport");
+        }
+    }
+    IEnumerator Teleport()
+    {
+        skillcool = true;
+        Debug.Log("TP코드실행");
+        PV.RPC("TP", RpcTarget.AllBuffered);
+        yield return new WaitForSeconds(7f);
+
+        skillcool = false;
+        Debug.Log("스킬 재사용 가능!");
+    }
+    IEnumerator Dash()
+    {
+        skillcool = true;
+        Debug.Log("Dash코드실행");
+        PV.RPC("DASH", RpcTarget.AllBuffered);
+        yield return new WaitForSeconds(7f);
+
+        skillcool = false;
+        Debug.Log("스킬 재사용 가능!");
+    }
+
     #endregion
 
-    #region PunRPCFunction
+    #region PunRPC
+    //Dark의 스킬 구현
     [PunRPC]
-    IEnumerator CoolTimeCRT(float cool)
+    void TP()
     {
-        yield return new WaitForSeconds(cool);
-        PV.RPC("CoolTimeEnd", RpcTarget.AllBuffered);
+        Vector3 curPos = Dark.transform.position;
+        SpriteRenderer SR = Dark.GetComponent<SpriteRenderer>();
+        if (SR.flipX == true)
+        {
+            Dark.transform.position = curPos + new Vector3(-70f, 0, 0);
+        }
+        else if(SR.flipX == false)
+        {
+            Dark.transform.position = curPos + new Vector3(70f, 0, 0);
+        }
     }
 
+    //Light의 스킬 구현
     [PunRPC]
-    private void Teleport()
+    void DASH()
     {
-        Debug.Log("TP코드실행");
-        StartCoroutine(CoolTimeCRT(7f));
+        SpriteRenderer SR = Light.GetComponent<SpriteRenderer>();
+        Rigidbody2D RD = Light.GetComponent<Rigidbody2D>();
+
+        if (SR.flipX == true)
+        {
+            RD.AddForce(new Vector2(DashPower, 20), ForceMode2D.Impulse);
+        }
+        else if (SR.flipX == false)
+        {
+            RD.AddForce(new Vector2(DashPower, 20), ForceMode2D.Impulse);
+        }
     }
 
-    [PunRPC]
-    private void Dash()
-    {
-        Debug.Log("Dash코드실행");
-        StartCoroutine(CoolTimeCRT(7f));
-    }
     #endregion
 }
